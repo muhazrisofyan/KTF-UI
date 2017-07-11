770cea2b00ca0c0644d8b884bd8bd970b913de58
Muh Azri Sofyan <muhazrisofyan@gmail.com>
2017-07-11 12:49:45 +0700

    fix login logout admin

--- resources/views/auth/masterAdmin.blade.php
+++ resources/views/auth/masterAdmin.blade.php
@@ -61,7 +61,11 @@
                 <!-- /.logout -->
                 <li><a href="{{ route('logout') }}"
                     onclick="event.preventDefault();
-                             document.getElementById('logout-form').submit();"><i class="fa fa-sign-out fa-fw"></i> Logout</a></li>
+                             document.getElementById('logout-form').submit();"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                 {{ csrf_field() }}
+                             </form>
+                </li>
 
                 <!-- /.dropdown -->
             </ul>
