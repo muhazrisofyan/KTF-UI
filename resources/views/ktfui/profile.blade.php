2063ce128ca54d7ca54d856c2df0355f28fdd4c5
Hizkia Nugroho <hiznugroho@gmail.com>
2017-08-21 11:02:26 +0700

    update bagian profile

--- resources/views/ktfui/profile.blade.php
+++ resources/views/ktfui/profile.blade.php
@@ -7,14 +7,8 @@
       <div class="w3-container w3-white">
 
         {{-- Start introduction --}}
-        <div class="w3-content" style="padding-top:20px">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-            in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-            sint occaecat cupidatat non proident, sunt in culpa qui officia
-            deserunt mollit anim id est laborum.</p>
+        <div class="w3-content" style="padding-top:20px;text-align:justify">
+          <p>Tari tradisional Indonesia mencerminkan kekayaan dan keanekaragaman bangsa Indonesia. Beberapa tradisi seni tari seperti; tarian Bali, tarian Jawa, tarian Sunda, tarian Minangkabau, tarian Palembang, tarian Melayu, tarian Aceh, dan masih banyak lagi adalah seni tari yang berkembang sejak dahulu kala, meskipun demikian tari ini tetap dikembangkan hingga kini. Beberapa tari mungkin telah berusia ratusan tahun, sementara beberapa tari berlanggam tradisional mungkin baru diciptakan kurang dari satu dekade yang lalu. Penciptaan tari dengan koreografi baru, tetapi masih di dalam kerangka disiplin tradisi tari tertentu masih dimungkinkan. Sebagai hasilnya, muncullah beberapa tari kreasi baru. Tari kreasi baru ini dapat merupakan penggalian kembali akar-akar budaya yang telah sirna, penafsiran baru, inspirasi atau eksplorasi seni baru atas seni tari tradisional.</p>
         </div>
         {{-- End of Introduction --}}
 
@@ -38,9 +32,78 @@
       {{-- End of Coach Profile --}}
 
       {{-- Start dance description --}}
+      <div class="w3-container w3-content" style="margin-bottom:50px">
+        <h1 class="w3-stackyard w3-text-orange">List of Traditional Dance</h1>
+
+        <ul class="w3-ul" style="text-align:justify">
+          <li>Jill
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+               ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+               aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+               in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+               Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+               deserunt mollit anim id est laborum.
+             </p>
+          </li>
+          <li>Eve
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+               ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+               aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+               in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+               Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+               deserunt mollit anim id est laborum.
+             </p>
+          </li>
+          <li>Adam</li>
+        </ul>
+      </div>
       {{-- End of dance description --}}
 
       {{-- Start Picture and Video --}}
+      <div class="w3-container w3-content">
+        <h1 class="w3-stackyard w3-text-orange">Gallery</h1>
+        <ul class="w3-ul">
+          <li>
+            <div class="w3-row">
+                <div class="w3-half w3-card-2 w3-content w3-padding">
+                  <img class="w3-image" src="img/1.jpg" alt="Norway">
+                  <div class="w3-container w3-center">
+                    <p>The Troll's tongue in Hardanger, Norway</p>
+                  </div>
+                </div>
+                <div class="w3-half w3-card-2 w3-content w3-padding">
+                  <img class="w3-image" src="img/2.jpg" alt="Norway">
+                  <div class="w3-container w3-center">
+                    <p>The Troll's tongue in Hardanger, Norway</p>
+                  </div>
+                </div>
+            </div>
+          </li>
+          <li>
+            <div class="w3-row">
+                <div class="w3-half w3-card-2 w3-content w3-padding">
+                  <img class="w3-image" src="img/1.jpg" alt="Norway">
+                  <div class="w3-container w3-center">
+                    <p>The Troll's tongue in Hardanger, Norway</p>
+                  </div>
+                </div>
+                <div class="w3-half w3-card-2 w3-content w3-padding">
+                  <img class="w3-image" src="img/2.jpg" alt="Norway">
+                  <div class="w3-container w3-center">
+                    <p>The Troll's tongue in Hardanger, Norway</p>
+                  </div>
+                </div>
+            </div>
+          </li>
+          <li>
+            <div class="w3-row w3-content w3-container">
+              <iframe src="https://www.youtube.com/watch?v=lp-EO5I60KA" width="720" height="360"></iframe>
+            </div>
+          </li>
+        </ul>
+      </div>
       {{-- End of Picture and Video --}}
 
       </div>
